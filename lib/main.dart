41ef8ff57532d940f66e6d9d695dbdfd6ca9dea0
Caleb Neumann <>
2023-10-05 19:56:19 -0500

    app now has a custom icon and name on phone home screen

---
+++ lib/main.dart
@@ -0,0 +1,276 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+  var colors = ["Red", "Blue", "Green"];
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+  void deleteList(){
+    favorites.clear();
+  }
+  void deleteSingle(var word){
+    favorites.remove(word);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  final List _screens = [
+    {"screen": const GeneratorPage(), "title": "\n"}, //app crashes if the "title" object is absent
+    {"screen": const FavoritesPage(), "title": "\n"},
+    {"screen": const SettingsPage(), "title": "\n"},
+  ];
+
+  void _selectScreen(int value){
+    setState((){ //changes the page to the selected one
+      selectedIndex = value;
+
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(    
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("We might add something here?",
+        style: TextStyle(
+          color: Colors.white
+        ),),
+        
+        backgroundColor: Colors.pink,
+      ),
+      body: _screens[selectedIndex]["screen"],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: _selectScreen,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: "Calendar"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
+
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  const GeneratorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: const Text('Like'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: const Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatefulWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return const Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: IconButton(
+              icon: const Icon(Icons.delete_forever),
+              onPressed: (){
+                appState.deleteSingle(pair);
+                setState(() {});
+
+              },
+              ),
+            
+            // leading: Icon(Icons.delete),
+            title: Text(pair.asLowerCase),
+          ),
+          
+          ElevatedButton.icon(
+            onPressed: () {
+              appState.deleteList();
+              setState(() {});
+            },
+              icon: const Icon( // <-- Icon
+              Icons.delete,
+              size: 24.0,
+              
+            ),
+            label: const Text('Delete List'), // <-- Text
+          ),
+      ],
+    );
+  }
+}
+
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+  
+    return ListView(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(20),
+          child: Text('Settings',
+              style: TextStyle(fontSize: 40),
+              ),
+        ),
+        // for (var color in appState.colors)
+          ListTile(
+            leading: TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            onPressed: () {},
+            child: const Text('Enabled'),
+          ),
+            
+            // leading: Icon(Icons.delete),
+            // title: Text(color),
+          ),
+          
+      ],
+    );
+  }
+}
