97dd59df324262345310c53fcda65ca1f41d967b
Tyler Braun <tyler.braun@toyota.com>
2023-10-10 14:16:47 -0500

    prettier config

--- lib/calendarPage.dart
+++ lib/calendarPage.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'models.dart';
 
-
-
 class calendarPage extends StatefulWidget {
   const calendarPage({super.key});
 
@@ -26,36 +24,33 @@ class _calendarPageState extends State<calendarPage> {
       children: [
         Padding(
           padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
+          child: Text('You have ' '${appState.favorites.length} favorites:'),
         ),
         for (var pair in appState.favorites)
           ListTile(
             leading: IconButton(
               icon: const Icon(Icons.delete_forever),
-              onPressed: (){
+              onPressed: () {
                 appState.deleteSingle(pair);
                 setState(() {});
-
               },
-              ),
-            
+            ),
+
             // leading: Icon(Icons.delete),
             title: Text(pair.asLowerCase),
           ),
-          
-          ElevatedButton.icon(
-            onPressed: () {
-              appState.deleteList();
-              setState(() {});
-            },
-              icon: const Icon( // <-- Icon
-              Icons.delete,
-              size: 24.0,
-              
-            ),
-            label: const Text('Delete List'), // <-- Text
+        ElevatedButton.icon(
+          onPressed: () {
+            appState.deleteList();
+            setState(() {});
+          },
+          icon: const Icon(
+            // <-- Icon
+            Icons.delete,
+            size: 24.0,
           ),
+          label: const Text('Delete List'), // <-- Text
+        ),
       ],
     );
   }
