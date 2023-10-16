a438c9d982789a490f2e16d23b048c375cf6b08a
malcolmcase97 <malcolm.case.97@gmail.com>
2023-10-15 22:59:41 -0500

    Updated UserSettings, made settingsPage look nicer

--- lib/settingsPage.dart
+++ lib/settingsPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
@@ -11,29 +10,40 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-  
     return ListView(
       children: [
         const Padding(
           padding: EdgeInsets.all(20),
-          child: Text('Settings',
-              style: TextStyle(fontSize: 40),
-              ),
+          child: Text(
+            'Settings',
+            style: TextStyle(fontSize: 40),
+          ),
         ),
         // for (var color in appState.colors)
-          ListTile(
-            leading: TextButton(
+        ListTile(
+          leading: TextButton(
             style: TextButton.styleFrom(
-              textStyle: const TextStyle(fontSize: 20),
+              textStyle: const TextStyle(fontSize: 25),
             ),
             onPressed: () {},
-            child: const Text("This doesn't do anything"),
+            child: const Text("Theme"),
           ),
-            
-            // leading: Icon(Icons.delete),
-            // title: Text(color),
+
+          // leading: Icon(Icons.delete),
+          // title: Text(color),
+        ),
+        ListTile(
+          leading: TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 25),
+            ),
+            onPressed: () {},
+            child: const Text("Text Size"),
           ),
-          
+
+          // leading: Icon(Icons.delete),
+          // title: Text(color),
+        ),
       ],
     );
   }
