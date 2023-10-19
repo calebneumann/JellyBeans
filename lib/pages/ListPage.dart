dfc685bd5481375ac6eb11116a58dcc77cd31e10
KevinFham <kevin.pha01@gmail.com>
2023-10-19 15:30:58 -0500

    sorting and assignment widget formatting

--- lib/pages/ListPage.dart
+++ lib/pages/ListPage.dart
@@ -9,14 +9,6 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    // var pair = appState.current;
-
-    // IconData icon;
-    // if (appState.favorites.contains(pair)) {
-    //   icon = Icons.favorite;
-    // } else {
-    //   icon = Icons.favorite_border;
-    // }
 
     return ListView(
       //TODO: Iterate over some assignment list for these
