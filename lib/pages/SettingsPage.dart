331bfaf08321b9ec71905b46c970a39f59226c41
Tyler Braun <tyler.braun@toyota.com>
2023-10-25 15:48:10 -0500

    Bonnie pops up

--- lib/pages/SettingsPage.dart
+++ lib/pages/SettingsPage.dart
@@ -3,21 +3,19 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-
-
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
 }
-class _SettingsPageState extends State<SettingsPage>{
+
+class _SettingsPageState extends State<SettingsPage> {
   final player = AudioPlayer();
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        
         const Padding(
           padding: EdgeInsets.all(20),
           child: Text(
@@ -55,11 +53,19 @@ class _SettingsPageState extends State<SettingsPage>{
             style: TextButton.styleFrom(
               textStyle: const TextStyle(fontSize: 25),
             ),
-            onPressed: () async{ 
-              await player.play(AssetSource('audio/FNAF.mp3')); //plays a silly FNAF noise LOL GOTTEM
-              }, 
-            child: const Text("FNAF JUMPSCARE \n(only sound, need to add gif)",
-            style: TextStyle(fontSize: 20),),
+            onPressed: () async {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                      content: Image(
+                          image: AssetImage('assets/images/BONNIE.gif'))));
+              await player.play(AssetSource(
+                  'audio/FNAF.mp3')); //plays a silly FNAF noise LOL GOTTEM
+            },
+            child: const Text(
+              "FNAF JUMPSCARE \n(only sound, need to add gif)",
+              style: TextStyle(fontSize: 20),
+            ),
           ),
         ),
       ],
