290f5989484e44b24015b8a1a5f6913bd1a427cb
Caleb Neumann <143125128+calebneumann@users.noreply.github.com>
2023-11-20 02:33:50 -0600

    added some comments for wittle ol' me

--- lib/models/Themes.dart
+++ lib/models/Themes.dart
@@ -4,5 +4,11 @@
 //somehow figure out how to add an RGB slider but we'll see...
 //...we shall see...
 
+//text should be black or white depending on the brightness of the theme
+  //or always make background brighter and the appbar/navbar darker
+  //like a colorful sandwich
 
+//start with presets and get those functional, then do RGB panel
+  //perfecting an RGB selected theme will be hard because of what needs to 
+  //change depending on if the color is bright/dark/clashes with both white and black text
 class poop{}
